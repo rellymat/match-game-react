bbd38c4d6529ba71a57b25e750463949526d4d11
rellymat <48412306+rellymat@users.noreply.github.com>
2021-03-13 19:04:20 +0200

    Add files via upload

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,1 +1,16 @@
+import jwt from 'jsonwebtoken'
 
+  
+
+ export function auth(req, res, next) {
+    const token = req.header("auth-token");
+    if (!token) return res.status(401).send("Access denied");
+
+    try {
+      const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
+      req.user = decoded;
+      next();
+    } catch (ex) {
+      res.status(400).send("Invalid token.");
+    }
+  };
